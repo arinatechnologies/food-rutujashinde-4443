5e86a536334f3ef02ff6db525bfc6cb215cc2964
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 07:56:45 -0500

    Upload from Lambda

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+{
+  "images": [
+    "modern restaurant interior with tables",
+    "chef preparing food in kitchen",
+    "close-up of gourmet dish",
+    "restaurant exterior with signage",
+    "happy customers dining together"
+  ]
+}
